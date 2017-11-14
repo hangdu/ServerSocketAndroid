8ff75d1481c725763206e4c0a8abf6cf8ee7317e
hangdu <2580392278@qq.com>
2017-11-14 13:32:48 +0800

    first version

--- app/src/main/java/com/example/hang/socketserver/MainActivity.java
+++ app/src/main/java/com/example/hang/socketserver/MainActivity.java
@@ -2,20 +2,58 @@ package com.example.hang.socketserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button startCollect;
+    Button stopCollect;
+    List<Socket> socketList = new ArrayList<Socket>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textview);
+        startCollect = (Button) findViewById(R.id.startCollect);
+        stopCollect = (Button) findViewById(R.id.stopCollect);
 
-        SocketServerDemo demo = new SocketServerDemo();
-        demo.start();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket server = new ServerSocket(12345);
+                    while(true){
+                        Socket s = server.accept();
+                        socketList.add(s);
+                        //每当客户端连接之后启动一条ServerThread线程为该客户端服务
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(runnable).start();
+
+        startCollect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Button is clicked!");
+                if (socketList.size() == 0) {
+                    return;
+                }
+                new Thread(new SocketServerDemo(textView, 0, socketList.get(0))).start();
+                socketList.remove(0);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hang/socketserver/SocketServerDemo.java
+++ app/src/main/java/com/example/hang/socketserver/SocketServerDemo.java
@@ -1,5 +1,9 @@
 package com.example.hang.socketserver;
 
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -11,24 +15,63 @@ import java.net.Socket;
  */
 
 public class SocketServerDemo extends Thread {
+    TextView textView;
+    int command;
+    Socket s;
+    public SocketServerDemo(TextView textView, int command, Socket s) {
+        this.textView = textView;
+        this.command = command;
+        this.s = s;
+    }
+
+    public Handler myHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0) {
+                textView.setText("new client joins in");
+            } else if (msg.what == 1) {
+                textView.setText("receive data from client:" + msg.obj);
+            } else {
+                textView.setText("something wrong");
+            }
+        }
+
+    };
     @Override
     public void run() {
         // TODO Auto-generated method stub
         super.run();
-        ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(12345);
-            while (true) {
-                // 等待客户端请求，如果没有客户端请求，会一直堵塞在这里
-                Socket s = serverSocket.accept();
-                OutputStream os = s.getOutputStream();
-                os.write("Hello client".getBytes());
-                os.close();
-                s.close();
-            }
-        } catch (IOException e1) {
-            e1.printStackTrace();
+            myHandler.sendEmptyMessage(0);
+            //command==0 means:  start to collect data
+            //command==1 means:  stop to collect data
+            OutputStream os = s.getOutputStream();
+            os.write(command);
+            os.flush();
 
+            //receive
+            InputStream in = s.getInputStream();
+//            myHandler.sendEmptyMessage(1);
+
+            byte[] b = new byte[32];
+            int count = in.read(b);
+            byte temp[] = new byte[count];
+            for (int i = 0; i < count; i++) {
+                temp[i] = b[i];
+            }
+            String str =  new String(temp);
+            Message msg = new Message();
+            msg.what = 1;
+            msg.obj = str;
+            myHandler.sendMessage(msg);
+
+            os.close();
+            in.close();
+
+            s.close();
+        } catch (IOException e1) {
+            myHandler.sendEmptyMessage(2);
+            e1.printStackTrace();
         }
     }
 }
