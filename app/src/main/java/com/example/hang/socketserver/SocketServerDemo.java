9f7273f4bb7d1f9190dfd83dea9ff8744700d16e
hangdu <2580392278@qq.com>
2017-11-20 10:57:00 +0800

    Client can send heartbeat. Server can detect if no heartbeat.

--- app/src/main/java/com/example/hang/socketserver/SocketServerDemo.java
+++ app/src/main/java/com/example/hang/socketserver/SocketServerDemo.java
@@ -3,15 +3,22 @@ package com.example.hang.socketserver;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.SystemClock;
 import android.widget.TextView;
 
+import com.orhanobut.logger.Logger;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.Queue;
+
 
 /**
  * Created by hang on 11/13/17.
@@ -20,21 +27,167 @@ import java.util.Queue;
 public class SocketServerDemo extends Thread {
     TextView textView;
     TextView status_textview;
-//    int command;
-    Socket s;
+    Socket mSocket;
     InputStream in;
     OutputStream os;
+    long lastReceiveHeartbeatTime;
 
     boolean isReceive = false;
+    SocketReadThread readThread;
     Queue<Integer> commandQueue = new LinkedList<>();
     public SocketServerDemo(TextView textView, TextView status_textview, Socket s) {
         this.textView = textView;
         this.status_textview = status_textview;
-        this.s = s;
+        this.mSocket = s;
+        lastReceiveHeartbeatTime = System.currentTimeMillis();
+
+        new Thread(monitorHeartBeat).start();
     }
 
     public void sendCommand(int command) {
         commandQueue.add(command);
+    }
+
+
+    /**
+     * 断开连接
+     *
+     */
+    private void releaseLastSocket() {
+        try {
+            if (null != mSocket) {
+                if (!mSocket.isClosed()) {
+                    mSocket.close();
+                }
+            }
+            mSocket = null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //There should be two thread
+    //One thread is used to receive
+    //Another thread is used to send
+
+
+
+//    如果主线程接收到的是心跳包，将该客户端对应的计数器 count 清零；
+//    在子线程中，每隔3秒遍历一次所有客户端的计数器 count：
+    Runnable monitorHeartBeat = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                if (System.currentTimeMillis() - lastReceiveHeartbeatTime > 12000) {
+                    Logger.d("SocketServerDemo", "Not receiving heartbeat on time. Closing this socket");
+                    Message msg = new Message();
+                    msg.what = 7;
+                    msg.obj = "Not receiving heartbeat on time. Closing this socket";
+                    myHandler.sendMessage(msg);
+                    releaseLastSocket();
+
+                    return;
+                }
+
+                try {
+                    sleep(12000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    };
+
+    public class SocketWriteThread extends Thread {
+        private static final String TAG = "SocketWriteThread";
+        private volatile boolean mStopThread = false;
+        public void release() {
+            mStopThread = true;
+        }
+
+        @Override
+        public void run() {
+            DataOutputStream mOutputStream = null;
+            try {
+                mOutputStream = new DataOutputStream(mSocket.getOutputStream());
+                while (mStopThread) {
+                    while (!commandQueue.isEmpty()) {
+                        int curCommand = commandQueue.poll();
+                        mOutputStream.writeBytes(String.valueOf(curCommand));
+                    }
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (mOutputStream != null) {
+                    try {
+                        mOutputStream.close();
+                        mOutputStream = null;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public class SocketReadThread extends Thread {
+        private static final String TAG = "SocketReadThread";
+        private volatile boolean mStopThread = false;
+
+        public void release() {
+            mStopThread = true;
+            releaseLastSocket();
+        }
+        @Override
+        public void run() {
+            DataInputStream mInputStream = null;
+            try {
+                mInputStream = new DataInputStream(mSocket.getInputStream());
+                Logger.d(TAG, "SocketThread running!");
+                while (!mStopThread) {
+
+
+                    String resultStr = mInputStream.readUTF();
+                    Logger.d(TAG, "read:" + resultStr);
+                    if (resultStr.equals("heartbeat")) {
+                        //receive heartBeat
+                        myHandler.sendEmptyMessage(5);
+                        lastReceiveHeartbeatTime = System.currentTimeMillis();
+                    } else  {
+                        //signal strength data
+                        Message msg = new Message();
+                        msg.what = 1;
+                        msg.obj = resultStr;
+                        myHandler.sendMessage(msg);
+                    }
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (mSocket != null) {
+                        mSocket.close();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (mInputStream != null) {
+                    try {
+                        mInputStream.close();
+                        mInputStream = null;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
     public Handler myHandler = new Handler(Looper.getMainLooper()) {
@@ -50,82 +203,32 @@ public class SocketServerDemo extends Thread {
                 textView.setText("send command 1");
             } else if (msg.what == 4) {
                 status_textview.setText("Client is not connected.");
+            } else if (msg.what == 5) {
+                status_textview.setText("HeatBeart");
+            } else if (msg.what == 6) {
+                status_textview.setText("receive " + msg.obj);
+            } else if (msg.what == 7) {
+                status_textview.setText(msg.obj + "!");
             }
 
             else {
                 textView.setText("something wrong");
             }
         }
-
     };
+
+
+
     @Override
     public void run() {
         // TODO Auto-generated method stub
         super.run();
         myHandler.sendEmptyMessage(0);
-        while (true) {
-            while (!commandQueue.isEmpty()) {
-                int curCommand = commandQueue.poll();
-                try {
-                    os = s.getOutputStream();
-                    os.write(String.valueOf(curCommand).getBytes());
-                    os.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        Logger.d("SocketServer", "Testing");
 
-                if (curCommand == 1) {
-                    isReceive = true;
-                    myHandler.sendEmptyMessage(3);
-                    Runnable runnable= new Runnable() {
-                        @Override
-                        public void run() {
-                            while (isReceive) {
-                                try {
-                                    in = s.getInputStream();
-                                    byte[] b = new byte[32];
-                                    int count = in.read(b);
-                                    if (count <= 0) {
-                                        //the socket is closed from the client side.  In this case, the server need to release the resources of the socket
-                                        os.close();
-                                        in.close();
-                                        s.close();
-                                        return;
-                                    }
-                                    //check the value of count so that the server know whether the client close the socket or not
-                                    byte temp[] = new byte[count];
-                                    for (int i = 0; i < count; i++) {
-                                        temp[i] = b[i];
-                                    }
-                                    String str =  new String(temp);
-                                    Message msg = new Message();
-                                    msg.what = 1;
-                                    msg.obj = str;
-                                    myHandler.sendMessage(msg);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    };
-                    new Thread(runnable).start();
-                } else {
-                    myHandler.sendEmptyMessage(2);
-                    isReceive = false;
-                }
-            }
-        }
+        readThread = new SocketReadThread();
+        readThread.start();
 
 
-        //support to send command continuously
-
-//        try {
-//            os.close();
-//            in.close();
-//            s.close();
-//        } catch (IOException e1) {
-//            myHandler.sendEmptyMessage(2);
-//            e1.printStackTrace();
-//        }
     }
 }
